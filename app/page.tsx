236dc41bd74bbc3450238c631423c6f7c46beacd
pouya <pouyamadankar@gmail.com>
2026-06-03 19:57:37 +0330

    FAQ

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import  Landing  from "./components/Landing"
 import Benefits from "./components/Benefits";
 import Structure from "./components/Structure"
 import Certifications from "./components/Certifications";
+import FAQ from "./components/FAQ"
 export default function Home() {
   return (
     <div dir="rtl" className="flex flex-col flex-1 items-center justify-center bg-zinc-50 ">
@@ -9,6 +10,7 @@ export default function Home() {
       <Benefits />
       <Structure />
       <Certifications />      
+      <FAQ />
     </div>
   );
 }
