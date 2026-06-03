0a00f36a2b34f39022e10a51ed9107e51e8bf479
pouya <pouyamadankar@gmail.com>
2026-06-03 20:10:06 +0330

    biuld

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/biomemetic",
+  assetPrefix: "/biomemetic/",
 };
 
 export default nextConfig;
