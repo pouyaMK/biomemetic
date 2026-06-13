f07af7fb143f9f133ba0c6649dbbea169d60d307
pouya <pouyamadankar@gmail.com>
2026-06-13 15:33:29 +0330

    WIP: change postgress after this commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -14,7 +14,7 @@ export default function AdminPage() {
     fetch("http://localhost:5000/api/phones")
       .then((res) => res.json())
       .then((data) => {
-        setPhones(data.data);
+        setPhones(data.phone);
       });
   }, []);
 
