e28fc8d5e60002429da72248c251c479dfacaba2
pouya <pouyamadankar@gmail.com>
2026-06-13 17:27:48 +0330

    delete button

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-
+import { FaTrash } from "react-icons/fa";
 import { useEffect, useState } from "react";
 
 interface PhoneData {
+  id: number;
   phone: string;
   feature: string;
   createdAt?: string;
@@ -28,20 +29,41 @@ export default function AdminPage() {
     fetchPhones();
   }, []);
 
+
+
+  const deletePhone = async (id: number) => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/phone/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+  
+      const data = await res.json();
+  
+      if (data.success) {
+        setPhones((prev) => prev.filter((item) => item.id !== id));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 p-6 md:p-10">
+    <div className="min-h-screen bg-linear-to-br from-slate-50 to-blue-50 p-6 md:p-10">
       <div className="max-w-5xl mx-auto">
         <div className="mb-10">
-          <h1 className="text-4xl font-black text-slate-900">
+          <h1 className="text-3xl text-right font-black text-slate-900">
             شماره‌های ثبت شده
           </h1>
 
-          <p className="text-slate-500 mt-2">
+          <p className="text-slate-500 text-sm mt-2 text-right">
             تعداد کل درخواست‌ها: {phones.length}
           </p>
         </div>
 
-        <div className="grid gap-5">
+        <div className="grid gap-3">
           {phones.map((item, index) => (
             <div
               key={index}
@@ -50,16 +72,29 @@ export default function AdminPage() {
                 border
                 border-slate-200
                 rounded-3xl
-                p-6
+                px-4
                 shadow-lg
                 hover:shadow-xl
                 transition-all
                 duration-300
               "
             >
-              <div className="flex items-start justify-between gap-4">
+            
+              <div className="flex items-center justify-between">
                 <div>
-                  <span
+      
+                  <h3 className="text-2xl font-black text-slate-900">
+                    {item.phone}
+                  </h3>
+
+                  <p className=" text-slate-600 text-sm">
+                    {item.feature}
+                  </p>
+                </div>
+
+              <section className="flex my-2 items-center justify-center flex-col gap-2">
+  
+              <span
                     className="
                       inline-flex
                       px-3
@@ -67,49 +102,24 @@ export default function AdminPage() {
                       rounded-full
                       text-xs
                       font-bold
-                      bg-blue-100
-                      text-blue-700
-                      mb-3
+                      bg-orange-100
+                      text-orange-500
+                      
                     "
                   >
                     درخواست #{index + 1}
                   </span>
+                  <button
+                onClick={() => deletePhone(item.id)}
+                className=" px-2 py-2 w-fit rounded-xl bg-red-500 text-white font-bold hover:bg-red-600 transition"
+              >
+                  <FaTrash size={20} />
+              </button>
 
-                  <h3 className="text-2xl font-black text-slate-900">
-                    {item.phone}
-                  </h3>
+                  
+              </section>
 
-                  <p className="mt-2 text-slate-600">
-                    {item.feature}
-                  </p>
-                </div>
-
-                <div
-                  className="
-                    shrink-0
-                    text-right
-                    bg-slate-100
-                    px-4
-                    py-2
-                    rounded-2xl
-                  "
-                >
-                  <p className="text-xs text-slate-500">
-                    زمان ثبت
-                  </p>
-
-                  <p className="font-bold text-slate-800">
-                    {item.createdAt
-                      ? new Date(item.createdAt).toLocaleTimeString(
-                          "fa-IR",
-                          {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          }
-                        )
-                      : "--:--"}
-                  </p>
-                </div>
+               
               </div>
             </div>
           ))}
